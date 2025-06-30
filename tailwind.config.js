681f05bc2982e4b7fd2eff5320fb667879ab75db
Daniel Nazimov <64067988+RezyJS@users.noreply.github.com>
2025-06-30 10:09:56 +0300

    no

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,6 @@
 module.exports = {
   content: ['./**/*.{js,ts,jsx,tsx}'],
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            pre: {
-              backgroundColor: '',
-              padding: 0,
-            },
-          },
-        },
-      },
-    },
-  },
-  // eslint-disable-next-line no-undef
-  plugins: [require('@tailwindcss/typography')],
+  theme: {},
+  plugins: [],
 }
